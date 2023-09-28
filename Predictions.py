3ac2dd1ca558616da06d4e5829cd5841997541bd
Ounoughi-Chahinez <ounoughichahinez@gmail.com>
2023-09-28 12:24:14 +0300

    Predictions.py
    Correction of the Carbon Intensity calculation formula

--- Predictions.py
+++ Predictions.py
@@ -10,17 +10,27 @@ import datetime
 def get_shared_arg_parser():
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--elering_path', type=str, default='Data/elering-data.csv', help='Elering data path')
-    parser.add_argument('--weather_path', type=str, default='Data/weather-data.csv', help='Weather data path')
+    parser.add_argument('--elering_path', type=str, default='elering-data.csv', help='Elering data path')
+    parser.add_argument('--weather_path', type=str, default='weather-data.csv', help='Weather data path')
     parser.add_argument('--new_value', type=float, default=0, help='The new consumption value comming from the meter')
     parser.add_argument('--Meter_name', type=int,  help='The Meter name')
-    parser.add_argument('--d_path', type=str,default='Data/Meters_dictionary.csv', help='Meters codes Dictionary path')
-    parser.add_argument('--d_WC_path', type=str,default='Data/WeatherCondition_dictionary.csv', help='Weather Condition Dictionary path')
-    parser.add_argument('--d_WW_path', type=str,default='Data/WeatherWind_dictionary.csv', help='Weather Wind Dictionary path')
-    parser.add_argument('--model_path',type=str, default='Model/Model-CNNLSTM.h5', help='Model path')
-    parser.add_argument('--pred_path', type=str,default='Results/preds.csv', help='Save predictions path')
+    parser.add_argument('--d_path', type=str,default='Meters_dictionary.csv', help='Meters codes Dictionary path')
+    parser.add_argument('--d_WC_path', type=str,default='WeatherCondition_dictionary.csv', help='Weather Condition Dictionary path')
+    parser.add_argument('--d_WW_path', type=str,default='WeatherWind_dictionary.csv', help='Weather Wind Dictionary path')
+    parser.add_argument('--model_path',type=str, default='Model-CNNLSTM.h5', help='Model path')
+    parser.add_argument('--pred_path', type=str,default='preds.csv', help='Save predictions path')
     
     return parser
+
+
+# scraping weather data:
+# def scrape_weather():
+ 
+    
+# scraping exchange forecast next 12 hours: https://www.nordpoolgroup.com/en/Market-data1/Dayahead/Flow1/Flow/EE/Hourly/?view=table
+# def scrape_forecasting():
+
+
 
 
 # Prepare the data for the prediction task:
@@ -47,45 +57,50 @@ def prepare_data(Meter_name, new_value, elering, weather, d, d_WC, d_WW):
     
 
 # Function that returns the next 12h consumption prediction values:
-def Future_Preditcion(Meter_name, new_value, elering, weather, d, d_WC, d_WW, model):
+def future_preditcion(Meter_name, new_value, elering, weather, d, d_WC, d_WW, model):
     Input_data = prepare_data(Meter_name, new_value, elering, weather, d, d_WC, d_WW)
     pred = model.predict(np.array(Input_data).reshape(1, 22, 1).astype(np.float32))
 #     print('\n\nNext 12h consumption predictions values: '+str(pred)+' of the meter: ' + str(Meter_name))
     return pred
 
 # Compute the CI for the next 12 hours based on the prediction of the energy:
-def CarbonIntensity(Meter_name, pred, elering, d):
+def carbon_intensity(pred, elering_pred):
     G = []
-    I = 0
-    E = 0
-    # Compute total imported and exported energy: otal minus exports, total positive imports
-    if elering['Soome'][0]>0:
-        I = I + elering['Soome'][0]
-    else:
-        E = E + elering['Soome'][0]
-        
-    if elering['Läti'][0]>0:
-        I = I + elering['Läti'][0]
-    else:
-        E = E + elering['Läti'][0]
-        
-    if elering['Venemaa(Leningrad)'][0]>0:
-        I = I + elering['Venemaa(Leningrad)'][0]
-    else:
-        E = E + elering['Venemaa(Leningrad)'][0]
-        
-    if elering['Venemaa(Pihkva)'][0]>0:
-        I = I + elering['Venemaa(Pihkva)'][0]
-    else:
-        E = E + elering['Venemaa(Pihkva)'][0]
+    F = []
     
-    F = elering['Tootmine'][0] - (elering['Tuuleparkide toodang'][0] + elering['Päikeseenergia toodang'][0])
-    
-    for i in pred: 
-        G.append((F + (I*0.1))/ (E+pred))
+    for i in range(elering_pred):
+        I = 0
+        E = 0
+        # Compute total imported and exported energy: otal minus exports, total positive imports
+        if elering_pred['Soome'][i]>0:
+            I = I + elering_pred['Soome'][0]
+        else:
+            E = E + elering_pred['Soome'][0]
+
+        if elering['Läti'][i]>0:
+            I = I + elering_pred['Läti'][i]
+        else:
+            E = E + elering_pred['Läti'][i]
+
+        if elering['Venemaa(Leningrad)'][i]>0:
+            I = I + elering_pred['Venemaa(Leningrad)'][i]
+        else:
+            E = E + elering_pred['Venemaa(Leningrad)'][i]
+
+        if elering['Venemaa(Pihkva)'][i]>0:
+            I = I + elering_pred['Venemaa(Pihkva)'][i]
+        else:
+            E = E + elering_pred['Venemaa(Pihkva)'][i]
+
+
+        F.append(elering_pred['Tootmine'][i] - (elering_pred['Tuuleparkide toodang'][i] + elering_pred['Päikeseenergia toodang'][i]))
+        G.append((F + (I*0.1))/ (abs(E)+pred[i]))
         
     return G
 
+
+# def recommendation(pred, CI):
+  
 
 def main(elering_path, weather_path, new_value, Meter_name, d_path, d_WC_path, d_WW_path,model_path, pred_path):
     
@@ -114,8 +129,8 @@ def main(elering_path, weather_path, new_value, Meter_name, d_path, d_WC_path, d
     # Upload the model:
     model = load_model(model_path)
     
-    pred = Future_Preditcion(Meter_name, new_value, elering, weather, d, d_WC, d_WW, model)
-    CI = CarbonIntensity(Meter_name, pred, elering, d)
+    pred = future_preditcion(Meter_name, new_value, elering, weather, d, d_WC, d_WW, model)
+    CI = carbon_intensity(pred, elering)
     predictions = pd.read_csv(pred_path)
     
     if len(predictions)>0:
@@ -130,11 +145,11 @@ def main(elering_path, weather_path, new_value, Meter_name, d_path, d_WC_path, d
         predictions.iloc[index,i] = pred[0][j]
         predictions.iloc[index,i+1] = CI[0][0][j]
         j = j + 1
-        
-    predictions.index = predictions.index + 1 
+
+    
+    predictions.index = predictions.index + 1 # shifting index
     predictions = predictions.sort_index()
     predictions.to_csv(pred_path, index=False)
-    
     return datetime.datetime.now(), Meter_name, pred, CI
 
 
@@ -142,4 +157,4 @@ def main(elering_path, weather_path, new_value, Meter_name, d_path, d_WC_path, d
 if __name__ == "__main__":
     parser = get_shared_arg_parser()
     args = parser.parse_args()
-    main(args.elering_path,args.weather_path,args.new_value,args.Meter_name,args.d_path,args.model_path, args.pred_path)
+    main(args.elering_path,args.elering_pred_path,args.weather_path,args.new_value,args.Meter_name,args.d_path,args.model_path, args.pred_path)
